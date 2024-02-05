06c65f0ddfcd06355b7ba6df2c3a4011f9b4a310
정원 <jwha94@gmail.com>
2024-02-06 03:44:54 +0900

    chore: remove unused code

--- src/hooks/products.ts
+++ src/hooks/products.ts
@@ -5,10 +5,7 @@ import { PaginationParams, PaginationResponse } from "../types/server";
 
 const productKeys = {
   all: ["users"] as const,
-  lists: () => [...productKeys.all, "list"] as const,
-  list: (filters: string) => [...productKeys.lists(), { filters }] as const,
-  details: () => [...productKeys.all, "detail"] as const,
-  detail: (id: number) => [...productKeys.details(), id] as const,
+  lists: () => [...productKeys.all, "list"] as const
 };
 
 export const useFetchProducts = ({ size }: PaginationParams) =>
@@ -16,10 +13,10 @@ export const useFetchProducts = ({ size }: PaginationParams) =>
     productKeys.lists(),
     ({ pageParam = 0 }: QueryFunctionContext) =>
       axios.get<PaginationResponse<Product>>("/products", {
-        params: { page: pageParam, size },
+        params: { page: pageParam, size }
       }),
     {
       getNextPageParam: ({ data: { isLastPage, pageNumber } }) =>
-        isLastPage ? undefined : pageNumber + 1,
+        isLastPage ? undefined : pageNumber + 1
     }
   );
